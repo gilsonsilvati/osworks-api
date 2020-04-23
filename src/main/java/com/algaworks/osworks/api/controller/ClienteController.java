1fa04dbef458f3871a7d5bbb1be093ec5f0473a8
Gilson Silva <gilsonsilvati@gmail.com>
2020-04-23 00:07:26 -0300

    Adicionando Migração de dados com Flyway e persistência de dados.

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,31 +1,23 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João das Couves 123");
-		cliente1.setTelefone("61 99999-2345");
-		cliente1.setEmail("joaodascouves@algaworks.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria da Silva");
-		cliente2.setTelefone("62 98765-2345");
-		cliente2.setEmail("mariadasilva@algaworks.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,11 +1,21 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@EqualsAndHashCode(of = { "id" })
 public class Cliente {
 	
-	@Getter @Setter
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
 	private Long id;
 	
 	@Getter @Setter
