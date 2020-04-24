f742f3c067359663112e5f6720e3d75bded31853
Gilson Silva <gilsonsilvati@gmail.com>
2020-04-24 11:22:28 -0300

    Alterando construtores.

--- src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -6,39 +6,32 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @JsonInclude(Include.NON_EMPTY)
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Problema {
 	
-	@NonNull
 	@Getter @Setter
 	private Integer status;
 	
-	@NonNull
 	@Getter @Setter
 	private LocalDateTime dataHora;
 	
-	@NonNull
 	@Getter @Setter
 	private String titulo;
 	
-	@NonNull
 	@Getter @Setter
 	private List<Campo> campos;
 	
-	@RequiredArgsConstructor
+	@AllArgsConstructor
 	public static class Campo {
 		
-		@NonNull
 		@Getter @Setter
 		private String nome;
 		
-		@NonNull
 		@Getter @Setter
 		private String mensagem;
 		
